53d2f9a615e9bd1f398c03a0f3b34c5b61c09510
BJC <bencochrane1@me.com>
2013-07-22 19:03:54 +1000

    Add in the new movie app

---
+++ 09_models_active_record/code_alongs/bcs_movie_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BcsMovieApp::Application.config.secret_key_base = '3cf2e7597194db52fb302ceaee49f1771f7b89818ad746f6ff7411567b039fa70244bd2dee3a4800a405fee6919148fe309fd8ffed37bad8020602997e80581a'
---
+++ 09_models_active_record/code_alongs/bcs_movie_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BcsMovieApp::Application.config.session_store :cookie_store, key: '_bcs_movie_app_session'
